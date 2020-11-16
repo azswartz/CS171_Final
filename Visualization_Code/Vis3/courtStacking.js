339fca10b45b02db6e39fc250cbfcf3cc2e1adba
Ralph Estanboulieh <ralphestanboulieh@dhcp-10-250-72-106.harvard.edu>
2020-11-15 19:18:51 -0500

    basics of vis-3

--- Visualization_Code/Vis3/courtStacking.js
+++ Visualization_Code/Vis3/courtStacking.js
@@ -11,7 +11,7 @@ class CourtStacking {
     initvis() {
         let vis = this;
 
-        vis.margin = {top: 20, right: 50, bottom: 50, left: 100};
+        vis.margin = {top: 20, right: 0, bottom: 20, left: 110};
         vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right;
         vis.height = $("#" + vis.parentElement).height() - vis.margin.top - vis.margin.bottom;
 
@@ -23,8 +23,8 @@ class CourtStacking {
 
         vis.y = d3.scaleBand()
             .domain(["senate", "president"])
-            .range([100, 0])
-            .paddingInner(0.2);
+            .range([80, 0])
+            .paddingInner(0.1);
 
         vis.yAxis = d3.axisLeft()
             .scale(vis.y)
@@ -41,6 +41,7 @@ class CourtStacking {
         vis.tiles.append("text")
             .attr("x", -10)
             .attr("y", vis.y("president") + vis.y.bandwidth()/2 )
+            .attr("class", "vis-3-y-label")
             .style("dominant-baseline", "central")
             .style("text-anchor", "end")
             .text("President")
@@ -48,6 +49,7 @@ class CourtStacking {
         vis.tiles.append("text")
             .attr("x", -10)
             .attr("y", vis.y("senate") + vis.y.bandwidth()/2 )
+            .attr("class", "vis-3-y-label")
             .style("dominant-baseline", "central")
             .style("text-anchor", "end")
             .text("Senate")
@@ -66,9 +68,44 @@ class CourtStacking {
     updateVis() {
         let vis = this;
 
-        vis.pad_w = vis.width * 0.03 / vis.displayData.length
+        vis.pad_w = vis.width * 0.03 / vis.displayData.length;
         vis.block_w = vis.width/vis.displayData.length;
         vis.block_h = 40;
+
+
+
+        vis.dot_n = 7;
+        vis.dot_inter = vis.block_w/(vis.dot_n + 1);
+        vis.dot_r = vis.dot_inter * 0.4;
+        vis.dot_inter_v = vis.dot_inter * 0.9
+
+        vis.tower_heights_px = vis.displayData.map(function (d) {
+            return Math.floor(d.referred / vis.dot_n) * vis.dot_inter_v
+        });
+
+        vis.max_towers_height = d3.max(vis.tower_heights_px);
+
+
+        vis.towers = vis.svg.selectAll("dots-tower")
+            .data(vis.displayData);
+
+        vis.tower_groups = vis.towers
+            .enter()
+            .append("g")
+            .merge(vis.towers)
+            .attr("class", "dot-tower")
+            .attr("transform", (d,i) => `translate(${i*vis.block_w}, 30)`);
+
+        vis.tower_groups.exit().remove();
+
+        vis.tower_groups.append("text")
+            .text(d => (d.fraction * 100).toFixed(0) + "%")
+            .attr("class", "dot-bar-fraction")
+            .attr("x", vis.block_w/2 + 2)
+            .style("text-anchor", "middle")
+            .style("dominant-baseline", "hanging")
+            .style("fill", d => d3.interpolatePlasma(1-d.fraction))
+            .attr("y", d => vis.max_towers_height + 12);
 
         vis.presidentBlocks = vis.tiles.selectAll(".president_blocks")
             .data(vis.displayData, d => d.congress);
@@ -80,7 +117,18 @@ class CourtStacking {
             .attr("class", "president_blocks")
             .attr("x", (d,i)=> i*vis.block_w)
             .attr("y", vis.y("president"))
-            .attr("width", vis.block_w - vis.pad_w)
+            .attr("width", 0.5 * (vis.block_w - vis.pad_w))
+            .attr("height", vis.y.bandwidth)
+            .style("fill", (d,i) => d.president == 'Democrat'? 'royalblue' : 'firebrick')
+
+        vis.presidentBlocks
+            .enter()
+            .append("rect")
+            .merge(vis.presidentBlocks)
+            .attr("class", "president_blocks")
+            .attr("x", (d,i)=> (i + 0.5)*vis.block_w)
+            .attr("y", vis.y("president"))
+            .attr("width", 0.5 * (vis.block_w - vis.pad_w))
             .attr("height", vis.y.bandwidth)
             .style("fill", (d,i) => d.president == 'Democrat'? 'royalblue' : 'firebrick')
 
@@ -96,11 +144,67 @@ class CourtStacking {
             .attr("class", "senate_blocks")
             .attr("x", (d,i)=> i*vis.block_w)
             .attr("y", vis.y("senate"))
-            .attr("width", vis.block_w - vis.pad_w)
+            .attr("width", 0.5*(vis.block_w - vis.pad_w))
             .attr("height", vis.y.bandwidth)
-            .style("fill", (d,i) => d.senate == 'Democrat'? 'royalblue' : 'firebrick')
+            .style("fill", (d,i) => d.senate == 'Democrat'? 'royalblue' : 'firebrick');
+
+        vis.senateBlocks
+            .enter()
+            .append("rect")
+            .merge(vis.senateBlocks)
+            .attr("class", "senate_blocks")
+            .attr("x", (d,i)=> (i + 0.5)*vis.block_w)
+            .attr("y", vis.y("senate"))
+            .attr("width", 0.5*(vis.block_w - vis.pad_w))
+            .attr("height", vis.y.bandwidth)
+            .style("fill", (d,i) => d.senate == 'Democrat'? 'royalblue' : 'firebrick');
+
+        vis.senateBlocks
+            .enter()
+            .append("text")
+            .attr("class", "vis-3-year")
+            .text(d => d3.timeFormat("%Y")(d.year))
+            .attr("x", (d,i)=> i*vis.block_w)
+            .attr("y", vis.y("senate") + vis.y.bandwidth() + 3)
+            .style("dominant-baseline", "hanging");
 
         vis.senateBlocks.exit().remove();
+
+        vis.tower_groups.selectAll(".tower-dot-ref")
+            .data(d => d3.range(d.referred))
+            .enter()
+            .append("circle")
+            .attr("cx", (d,i) => (i % vis.dot_n + 1) * vis.dot_inter)
+            .attr("cy", (d,i) => vis.max_towers_height - (Math.floor(i/vis.dot_n) * vis.dot_inter_v))
+            .attr("r", vis.dot_r)
+            .style("fill", "none")
+            .transition()
+            .duration(100)
+            .delay(d => 200 + d*2)
+            .attr("class", "tower-dot-ref")
+            .attr("cx", (d,i) => (i % vis.dot_n + 1) * vis.dot_inter)
+            .attr("cy", (d,i) => vis.max_towers_height - (Math.floor(i/vis.dot_n) * vis.dot_inter_v))
+            .attr("r", vis.dot_r)
+            .style("fill", "lightgrey");
+
+        vis.tower_groups.selectAll(".tower-dot-conf")
+            .data(d => d3.range(d.confirmed))
+            .enter()
+            .append("circle")
+            .attr("cx", (d,i) => (i % vis.dot_n + 1) * vis.dot_inter)
+            .attr("cy", (d,i) => vis.max_towers_height - (Math.floor(i/vis.dot_n) * vis.dot_inter_v))
+            .attr("r", vis.dot_r)
+            .style("fill", "none")
+            .transition()
+            .duration(100)
+            .delay(d => 200 + d*2)
+            .attr("class", "tower-dot-conf")
+            .attr("cx", (d,i) => (i % vis.dot_n + 1) * vis.dot_inter)
+            .attr("cy", (d,i) => vis.max_towers_height - (Math.floor(i/vis.dot_n) * vis.dot_inter_v))
+            .attr("r", vis.dot_r)
+            .style("fill", "darkseagreen");
+
+
 
     }
 }
