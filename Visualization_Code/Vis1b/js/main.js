e85d057cb5fcc840bc58a77aae77bef11028b433
Elliot Parlin <elliotparlin@Elliots-MBP.nycap.rr.com>
2020-11-20 21:21:32 -0500

    added Vis1b

---
+++ Visualization_Code/Vis1b/js/main.js
@@ -0,0 +1,19 @@
+//get data from the csv
+let barchart;
+d3.csv("data/states_pop.csv", (row) => {
+    for(let i = 1790; i < 2021; i += 10){
+        let year = "" + i;
+        row[year] = +row[year];
+    }
+    row.yearOfStatehood = +row.yearOfStatehood;
+    return row;
+}).then(data => {
+    //console.log(data);
+
+    //initialize visualization
+    barchart = new BarChart("barchart", data);
+});
+
+function updateVisualization(){
+    barchart.wrangleData();
+}
